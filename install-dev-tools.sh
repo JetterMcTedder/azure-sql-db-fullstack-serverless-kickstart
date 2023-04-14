aa74edddf75b8bda40012c92491d0a3f219ac068
Brian Spendolini <69256666+JetterMcTedder@users.noreply.github.com>
2023-04-14 12:27:51 -1000

    Update install-dev-tools.sh

--- install-dev-tools.sh
+++ install-dev-tools.sh
@@ -5,3 +5,6 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main\" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
 sudo apt-get install azure-functions-core-tools-4
+dab --version 
+func version 
+dotnet --version 
